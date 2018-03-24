2e1fb90bded94a9c8f5a2cceab3a80d4ca1a4bb9
Shaojung <shaojung@gmail.com>
2018-03-24 16:21:11 +0800

    DAO

--- app/src/main/java/com/example/student/db2018032402/DetailActivity.java
+++ app/src/main/java/com/example/student/db2018032402/DetailActivity.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    StudentDAO dao;
+    TextView tv, tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        dao = new StudentDAOImpl(DetailActivity.this);
         Intent it = getIntent();
         int id = it.getIntExtra("id", 0);
         Log.d("ID", String.valueOf(id));
+        Phone p = dao.getOneData(id);
+        tv = findViewById(R.id.textView);
+        tv2 = findViewById(R.id.textView2);
+        tv.setText(p.stuname);
+        tv2.setText(p.tel);
     }
 }
