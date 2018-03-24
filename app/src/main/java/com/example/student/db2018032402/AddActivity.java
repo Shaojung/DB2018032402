f5dae3326f22e8595b8bf7a7d1fe286e0ae80684
Shaojung <shaojung@gmail.com>
2018-03-24 14:06:56 +0800

    copy file

--- app/src/main/java/com/example/student/db2018032402/AddActivity.java
+++ app/src/main/java/com/example/student/db2018032402/AddActivity.java
@@ -1,18 +1,28 @@
 package com.example.student.db2018032402;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class AddActivity extends AppCompatActivity {
+import java.io.File;
 
+public class AddActivity extends AppCompatActivity {
+    File fout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        fout = new File(getFilesDir(), "student.sqlite");
     }
     public void clickAdd(View v)
     {
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(fout, null);
+        ContentValues cv = new ContentValues();
+        cv.put("stuname", "AAAA");
+        cv.put("tel", "1111");
+        db.insert("phone", null, cv);
 
     }
 }
